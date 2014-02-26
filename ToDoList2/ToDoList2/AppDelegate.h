1e9bcb870ae7c1f8d99e6377f0bf5524a96f3b12
elliciachen <elliica@kik.com>
2014-02-26 13:33:01 -0500

    Create To-Do List Foundation

---
+++ ToDoList2/ToDoList2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList2
+//
+//  Created by Ellicia Chen on 2/26/2014.
+//  Copyright (c) 2014 LaLuneRabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList2/ToDoList2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList2
+//
+//  Created by Ellicia Chen on 2/26/2014.
+//  Copyright (c) 2014 LaLuneRabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
